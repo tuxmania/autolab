6aa06e72cb318949b7db890f4c06caa47c87d3cf
Frédéric Stemmelin <fred@giga.(none)>
2012-07-07 23:02:45 +0200

    Toujours plus de cohérence dans le nom des fichiers de commandes

--- Automate/Floppy/VC/VCPhase1.cmd
+++ Automate/Floppy/VC/VCPhase1.cmd
@@ -4,5 +4,5 @@ echo * Connect to build share \\192.168.199.7\Build
 net use B: \\192.168.199.7\Build >> c:\buildlog.txt
 type b:\automate\version.tct  >> c:\buildlog.txt
 echo *
-echo * Transfer to vcbuild2.cmd 
-call b:\Automate\VC\VCBuild2.cmd
+echo * Transfer to VCPhase2.cmd 
+call b:\Automate\VC\VCPhase2.cmd
